5c980d0a55fc9da86207f809a86145407c135f62
fbraza <fbraza@tutanota.com>
2021-04-12 13:57:53 +0100

    test: add the test for DataFrame summary output

--- tests/test_summarize_dataframe.py
+++ tests/test_summarize_dataframe.py
@@ -1,4 +1,5 @@
 import unittest
+import pytest
 import pandas as pd
 
 
@@ -18,6 +19,10 @@ class TestDataSummary(unittest.TestCase):
         self.sum_col = ['Values']
         self.sum_idx = ['Number of rows', 'Number of columns', 'int64', 'object']
         self.sum_data = [[3], [2], [1], [1]]
+
+    @pytest.fixture(autouse=True)
+    def _pass_fixture(self, capsys):
+        self.capsys = capsys
 
     def test_shape(self):
         expected = pd.DataFrame(data=self.sh_data, columns=self.sum_col, index=self.sh_idx)
@@ -39,12 +44,20 @@ class TestDataSummary(unittest.TestCase):
         self.assertTrue(result.equals(expected))
 
     def test_data_summary(self):
-        expected = pd.DataFrame(data=self.sum_data, columns=self.sum_col, index=self.sum_idx)
+        # test the final dataframe
+        expected_df = pd.DataFrame(data=self.sum_data, columns=self.sum_col, index=self.sum_idx)
         data1, data2 = [[3], [2]], [[1], [1]]
         df_shape = pd.DataFrame(data=data1, columns=self.sum_col, index=self.sh_idx)
         df_freqs = pd.DataFrame(data=data2, columns=self.sum_col, index=self.freq_idx)
-        result = pd.concat([df_shape, df_freqs])
-        self.assertTrue(expected.equals(result))
+        result_df = pd.concat([df_shape, df_freqs])
+        self.assertTrue(expected_df.equals(result_df))
+        # test the output
+        message = '--- Data summary ---'
+        print(message, result_df, sep='\n')
+        result_stdout = self.capsys.readouterr()
+        print(message, expected_df, sep='\n')
+        expected_stdout = self.capsys.readouterr()
+        self.assertEqual(expected_stdout, result_stdout)
 
 
 if __name__ == '__main__':
